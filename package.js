1a374ca21a48a693ccf92dcf4e3f9e4092f38451
Jimmie Butler <jimmiebtlr@gmail.com>
2015-10-22 14:05:19 -0500

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'poetic:react-swiper',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'React-swipe npm package exposed on meteor client',
   documentation: 'README.md'
 });
